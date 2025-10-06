7f1bebb286830e5fef97444bc0f6db0debe16ce6
Ítalo S. Leão <italoleao.dev@gmail.com>
2025-10-05 22:42:59 -0300

    Update Questao01a.java

--- aula_pratica04/Questao01a.java
+++ aula_pratica04/Questao01a.java
@@ -1,4 +1,5 @@
 package aula_pratica4;
+
 class Questao01a {
     public static void main(String[] args) {
         int x = 37;
@@ -9,3 +10,4 @@ class Questao01a {
     //Cannot make a static reference to the non-static field x 
     //O código não foi executado corretamente, pois a variavel x esta em uma classe nao estatica, e comando de impressao
     //a referenciando estava na classe estatica, gerando uma incompatibilidade.
+
